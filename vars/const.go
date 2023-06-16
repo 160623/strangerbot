d47c029b24cd05d049133c6afc7a094485615b96
160623 <136820474+160623@users.noreply.github.com>
2023-06-16 21:38:24 +0800

    Update const.go
    ntumatchbot ... StartMatchingMessage 
    
    supportthebot patreon added

--- vars/const.go
+++ vars/const.go
@@ -10,7 +10,7 @@ var (
 )
 
 const (
-	StartMatchingMessage    = `Looking for another cool user to match you with... Hold on! (This may take a while, maybe even days as we strive for a perfect match! Keep your notifications on!) **NOTE: If you send anything illegal here, your data will be handed over to the authorities. You're anonymous only until you break the rules. Enter /report (with a reason) to report a user; e.g. /report advertising . If a chat with a user you want to report has already ended, do not start a new chat—immediately contact the admin.`
+	StartMatchingMessage    = `Looking for another user to match you with... Hold on! (This may take a while, maybe even days as we strive for a perfect match! Keep your notifications on! You may also use the @NTUchatbot in the meantime but it has no student verification/less specificity) **NOTE: If you send anything illegal here, your data will be handed over to the authorities. You're anonymous only until you break the rules. Enter /report (with a reason) to report a user; e.g. /report advertising . If a chat with a user you want to report has already ended, do not start a new chat—immediately contact the admin.`
 	NotProfileFinishMessage = "Press /setup to ensure everything has been configured in your profile and match settings before pressing /start"
 	TopMenuMessage          = `Set up both your profile and match settings here! You'll need to fully fill both before you can enter /start to join the queue. 💬`
 	HelpMessage             = `Help:
@@ -38,7 +38,7 @@ If you require any help, feel free to contact @aaldentnay !`
 	OTPSuccessMessage        = `Successfully registered! Please click /setup if you are not done setting up for the other options (including match settings), or /start to match.`
 	NotAccessRegisterMessage = `Sorry, you are not authorised to register.`
 	RetryInputEmailMessage   = `Mail service send failed, Please re-enter email.`
-	RateLimitMessage         = `You have hit your weekly limit! Wait a few days before trying again. Make a donation and contact @aaldentnay to be a paid user (you’ll be  able to increase the number of times you can press /start !)`
+	RateLimitMessage         = `You have hit your weekly limit! Wait a few days before trying again. Make a donation at https://Patreon.com/SupportTheBot (you’ll be  able to increase the number of times you can press /start or opt to get faster matches! This helps cover server/development costs.)`
 	InternalErrorMessage     = `An error occurred inside the robot`
 	ChangeGenderErrorMessage = `Gender change is not supported`
 )
